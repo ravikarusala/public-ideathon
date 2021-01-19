bc76e9b2f6f227a444a3d0094e905c93f45d1cd8
Raviteja Karusala <ravitejak@sumtotalsystems.com>
2021-01-19 08:17:06 -0500

    Checking in Public Folder

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+});
+
+function printDiv(divName) {
+    var printContents = document.getElementById(divName).outerHTML;
+    var originalContents = document.body.outerHTML;
+    document.body.outerHTML = printContents;
+    window.print();
+    document.body.outerHTML = originalContents;
+}
+
+function updateFileInput(val) {
+    document.getElementById('pictureURL').innerHTML = val.substr(12);
+  }
